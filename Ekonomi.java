074dbb6339c183befc77aa86330bc514f022e460
Ezar207 <155249223+Ezar207@users.noreply.github.com>
2024-01-11 20:18:37 +0700

    Update and rename Ekonomi.java to Pembeli.java

--- Ekonomi.java
+++
@@ -1,5 +0,0 @@
-class Ekonomi extends TiketKereta {
-    public Ekonomi() {
-        super("Ekonomi", 100000);
-    }
-}
---
+++ Pembeli.java
@@ -0,0 +1,9 @@
+public class Pembeli {
+    String nama;
+    int jumlahKursi;
+
+    public Pembeli(String nama, int jumlahKursi) {
+        this.nama = nama;
+        this.jumlahKursi = jumlahKursi;
+    }
+}
