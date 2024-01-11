c544149d9ca68260c1f8c0d1e51502120d6e5c7a
Ezar207 <155249223+Ezar207@users.noreply.github.com>
2024-01-11 20:18:04 +0700

    Update and rename Bisnis.java to Kereta.java

--- Bisnis.java
+++
@@ -1,5 +0,0 @@
-class Bisnis extends TiketKereta {
-    public Bisnis() {
-        super("Bisnis", 150000);
-    }
-}
---
+++ Kereta.java
@@ -0,0 +1,13 @@
+public class Kereta {
+    String jenis;
+    String tujuan;
+    String jam;
+    int harga;
+
+    public Kereta(String jenis, String tujuan, String jam, int harga) {
+        this.jenis = jenis;
+        this.tujuan = tujuan;
+        this.jam = jam;
+        this.harga = harga;
+    }
+}
